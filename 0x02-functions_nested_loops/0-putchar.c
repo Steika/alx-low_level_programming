bb9d9aea4140cd587deadecd80d030882db136e6
Ayoub-El-Fraoui <ayoub.elfraoui@gmail.com>
2023-03-21 14:28:17 -0700

    task8

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 /**
- * main - function that prints _putchar
- *
- * Return: always return 0
- *
- */
+* main - prints _putchar
+*
+* Return: 0
+*/
+
 int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
