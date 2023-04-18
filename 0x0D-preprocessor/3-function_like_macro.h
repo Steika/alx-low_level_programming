d9be658b843cf5c308a6ed6dab5ed58dc60bd259
Ayoub-El-Fraoui <ayoub.elfraoui@gmail.com>
2023-04-18 16:21:24 -0700

    preproc

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x > 0) ? x : -(x))
+#endif /* FUNCTION_LIKE_MACRO_H */
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
